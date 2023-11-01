0e9dd1da50391021e30208f0d64270c050967492
Kingcedru <byishimocedrick@gmail.com>
2023-11-01 10:59:54 +0200

    form inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-
 import "./App.css";
-import Exercise2 from "./components/Exercise2";
-
+import Form from "./components/Form";
 function App() {
   return (
     <>
-     <Exercise2/>
-
-import './App.css'
-import ListGroup from './components/ListGroup'
-
-function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  
-const handleSelectItem = (item:string)=>{
-  console.log(item)
-}
-
-  return (
-    <>
-      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/>
+      <Form/>
     </>
   );
 }
