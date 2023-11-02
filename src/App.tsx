e10f3cfd873671e70dcd6d032d71de02a4df3cba
Kingcedru <byishimocedrick@gmail.com>
2023-11-02 20:03:56 +0200

    fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css"; 
-import ProductList from "./components/ProductList";
-function App() {
+import axios from "axios";
+// import ProductList from "./components/ProductList";
+interface User{
+  id: number,
+  name:string,
+}
 
-  const [category,setCategory] = useState('')
+function App() {
+const [users, setUsers] = useState<User[]>([])
+const [error, setError] = useState('')
+useEffect(()=>{
+  axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  .then(res=>setUsers(res.data))
+  .catch(err=>setError(err.message))
+},[])
   return (
-    <>
-    <select className="form-select" onChange={(event)=> setCategory(event.target.value)}>
-      <option value=''></option>
-      <option value='Clothing'>Clothing</option>
-      <option value='HOusehold'>Household</option>
-    </select>
-      <ProductList category={category}/>
-    </>
+  <>
+  <ul>
+    {!error?users.map(user => <li key={user.id}>{user.name}</li>): <p className="text-danger">{error}</p>}
+  </ul>
+  </>
   );
 }
 
